7e8e2bf1d608683dc77bae872916f374bc9709a5
nancyhenry89 <nancyhenry89@gmail.com>
2017-09-12 21:32:48 +0200

    Initial

---
+++ scripts.js
@@ -0,0 +1,32 @@
+
+// After the API loads, call a function to enable the search box.
+function handleAPILoaded() {
+  $('#search-button').attr('disabled', false);
+}
+
+function search() {
+
+	console.log('Search Started');
+  var apiKey = 'AIzaSyCr7L91URLBfmHfXeUiKPnUbmL0s9gikSY';
+	var q = $('#query').val();
+	
+    gapi.client.setApiKey(apiKey);
+    gapi.client.load('youtube', 'v3', function() {
+        isLoad = true;
+    }); 
+	console.log('Search Request');
+	
+    request = gapi.client.youtube.search.list({
+				q: 'q',
+        part: 'id, snippet',
+        type: 'video',
+        order: 'date'
+     });
+    
+
+  request.execute(function(response) {
+    var str = JSON.stringify(response.result);
+    $('#search-container').html('<pre>' + str + '</pre>');
+  });
+} 
+
