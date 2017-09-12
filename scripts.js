72398df260a5887d8ffb7b491e59e37303ef429a
nancyhenry89 <nancyhenry89@gmail.com>
2017-09-12 22:54:57 +0200

    Client demo

--- scripts.js
+++ scripts.js
@@ -3,13 +3,15 @@
 function handleAPILoaded() {
   $('#search-button').attr('disabled', false);
 }
-
+/*function setCurrentTeam(){
+  sessionStorage.setItem('currentTeam', $(this).text());
+}*/
 function search() {
 
 	console.log('Search Started');
   var apiKey = 'AIzaSyCr7L91URLBfmHfXeUiKPnUbmL0s9gikSY';
-	var q = 'European Soccer '+$('#query').val();
-	
+	var q = 'European Soccer '+sessionStorage.getItem('currentTeam');;
+	$('#currentTeam').text(q)
     gapi.client.setApiKey(apiKey);
     gapi.client.load('youtube', 'v3', function() {
         isLoad = true;
@@ -32,3 +34,59 @@ function search() {
   });
 } 
 
+
+
+  // 2. This code loads the IFrame Player API code asynchronously.
+      var tag = document.createElement('script');
+
+      tag.src = "https://www.youtube.com/iframe_api";
+      var firstScriptTag = document.getElementsByTagName('script')[0];
+      firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+      // 3. This function creates an <iframe> (and YouTube player)
+      //    after the API code downloads.
+      var player;
+      function onYouTubeIframeAPIReady() {
+        player = new YT.Player('player', {
+          height: '390',
+          width: '640',
+          videoId: 'M7lc1UVf-VE',
+          events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange
+          }
+        });
+      }
+
+      // 4. The API will call this function when the video player is ready.
+      function onPlayerReady(event) {
+        event.target.playVideo();
+      }
+
+      // 5. The API calls this function when the player's state changes.
+      //    The function indicates that when playing a video (state=1),
+      //    the player should play for six seconds and then stop.
+      var done = false;
+      function onPlayerStateChange(event) {
+        if (event.data == YT.PlayerState.PLAYING && !done) {
+          setTimeout(stopVideo, 6000);
+          done = true;
+        }
+      }
+      function stopVideo() {
+        player.stopVideo();
+      }
+
+
+$(document).ready(function(){
+  $('.teamList a').click(function(){
+      sessionStorage.setItem('currentTeam', $(this).text());
+            $('.teamList').hide();
+      $('.matchList').show();
+      search();
+  });
+  $('#back').click(function(){
+      $('.matchList').hide();
+        $('.teamList').show();
+
+  });
+});
