fb9ce908691f76888fc3b5a4d1cfd54cc9c2d08b
nancyhenry89 <nancyhenry89@gmail.com>
2017-09-14 01:59:00 +0200

    Fix

--- scripts.js
+++ scripts.js
@@ -29,7 +29,7 @@ function search() {
   request.execute(function(response) {
     var str = response.result;
     sessionStorage.setItem('currentVid', str.items[0].id.videoId);
-    for (i=0;i<=str.items.length;i++){
+    for (i=0;i<str.items.length;i++){
        sessionStorage.setItem('videoID'+i, str.items[i].id.videoId);
     }
       changeVidId();
