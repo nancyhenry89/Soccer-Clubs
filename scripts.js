f74bc5b17a525988abd36c6f2e2e77dbe1f0808e
nancyhenry89 <nancyhenry89@gmail.com>
2017-09-14 10:17:30 +0200

    Son to string to save LS

--- scripts.js
+++ scripts.js
@@ -26,7 +26,7 @@ function search() {
 
   request.execute(function(response) {
     var str = response.result;
-    sessionStorage.setItem('result', str.items);
+    sessionStorage.setItem('result', JSON.stringify(str.items));
     sessionStorage.setItem('currentVid', str.items[0].id.videoId);
     for (i=0;i<str.items.length;i++){
        sessionStorage.setItem('videoID'+i, str.items[i].id.videoId);
