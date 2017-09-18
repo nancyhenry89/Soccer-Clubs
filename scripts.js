baa5b33c79f7c59c577b355277669030f904e6a3
nancyhenry89 <nancyhenry89@gmail.com>
2017-09-18 19:34:26 +0200

    Scope issue

--- scripts.js
+++ scripts.js
@@ -1,128 +1,131 @@
-
 // After the API loads, call a function to enable the search box.
 function handleAPILoaded() {
-  $('#search-button').attr('disabled', false);
+    $('#search-button').attr('disabled', false);
 }
 var str;
+
 function search() {
 
-  var apiKey = 'AIzaSyByr5hSWx-A9-Lai0SzDwqD6wavgF3xzgU';
-	var q = 'Soccer '+Cookies.get('currentTeam');
-   gapi.client.setApiKey(apiKey);
+    var apiKey = 'AIzaSyByr5hSWx-A9-Lai0SzDwqD6wavgF3xzgU';
+    var q = 'Soccer ' + Cookies.get('currentTeam');
+    gapi.client.setApiKey(apiKey);
 
-  gapi.load(function(){
-  })
-   
+    gapi.load(function() {})
+
 
     gapi.client.load('youtube', 'v3', function() {
         request = gapi.client.youtube.search.list({
-				q: q,
-        part: 'id, snippet',
-        type: 'video',
-        order: 'date',
-        maxResults:	'10',
-        videoEmbeddable:true,
+            q: q,
+            part: 'id, snippet',
+            type: 'video',
+            order: 'date',
+            maxResults: '10',
+            videoEmbeddable: true,
 
-     });
-    
+        });
 
-  request.execute(function(response) {
-     str = response.result;
-     var count;
-  var result=str.items;
- // localStorage.setItem('currentVid', result[0].id.videoId);
-        changeVidId(result[0].id.videoId,0);
-          $('#currentTeam').text(Cookies.get('currentTeam'));
-     for (i=0;i<result.length;i++){
-       $.getJSON('https://www.googleapis.com/youtube/v3/videos?part=statistics&id='+result[i].id.videoId+'&key=AIzaSyByr5hSWx-A9-Lai0SzDwqD6wavgF3xzgU', function(data) {
-    count=data.items[0].statistics.viewCount;
-  })
-       $('#list').append("<div num='"+i+"' id='"+result[i].id.videoId+"' class='vidItem'><span class='count'>View Count: <b>"+count+"</b>=</span><img src='"+result[i].snippet.thumbnails.default.url+"' /><span>"+result[i].snippet.title+"<span></div>")
-    }
-    $('#prev').click(function(){
-      if(parseInt($('#player').attr('num'))!=0){
-      var prev=result[parseInt($('#player').attr('num'))-1].id.videoId;
-      changeVidId(prev,parseInt($('#player').attr('num'))-1);
-      }
+
+        request.execute(function(response) {
+            str = response.result;
+            var count;
+            var result = str.items;
+            // localStorage.setItem('currentVid', result[0].id.videoId);
+            changeVidId(result[0].id.videoId, 0);
+            $('#currentTeam').text(Cookies.get('currentTeam'));
+            for (i = 0; i < result.length; i++) {
+                $.getJSON('https://www.googleapis.com/youtube/v3/videos?part=statistics&id=' + result[i].id.videoId + '&key=AIzaSyByr5hSWx-A9-Lai0SzDwqD6wavgF3xzgU', function(data) {
+                    count = data.items[0].statistics.viewCount;
+                    $('#list').append("<div num='" + i + "' id='" + result[i].id.videoId + "' class='vidItem'><span class='count'>View Count: <b>" + count + "</b>=</span><img src='" + result[i].snippet.thumbnails.default.url + "' /><span>" + result[i].snippet.title + "<span></div>")
+                })
+
+            }
+            $('#prev').click(function() {
+                if (parseInt($('#player').attr('num')) != 0) {
+                    var prev = result[parseInt($('#player').attr('num')) - 1].id.videoId;
+                    changeVidId(prev, parseInt($('#player').attr('num')) - 1);
+                }
+            });
+            $('#next').click(function() {
+                if (parseInt($('#player').attr('num')) != 9) {
+                    var next = result[parseInt($('#player').attr('num')) + 1].id.videoId;
+                    changeVidId(next, parseInt($('#player').attr('num')) + 1);
+                }
+            });
+            $("#list div").click(function() {
+                // localStorage.setItem('currentVid', $(this).attr('id'));
+                changeVidId($(this).attr('id'), parseInt($(this).attr('num')));
+            });
+
+
+        });
     });
-        $('#next').click(function(){
-    if(parseInt($('#player').attr('num'))!=9){
-      var next=result[parseInt($('#player').attr('num'))+1].id.videoId;
-      changeVidId(next,parseInt($('#player').attr('num'))+1);
-    }
-    });
-$("#list div").click(function(){
-   // localStorage.setItem('currentVid', $(this).attr('id'));
-      changeVidId($(this).attr('id'),parseInt($(this).attr('num')));
-});
 
-
-  });
-    }); 	
-
-
-} 
-
-function changeVidId(id,i){
-  $('#player').attr('src','https://www.youtube.com/embed/'+id)
-  $('#player').attr('num',i)
 
 }
-  
-  // 2. This code loads the IFrame Player API code asynchronously.
-      var tag = document.createElement('script');
 
-      tag.src = "https://www.youtube.com/iframe_api";
-      var firstScriptTag = document.getElementsByTagName('script')[0];
-      firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-      // 3. This function creates an <iframe> (and YouTube player)
-      //    after the API code downloads.
-      var player;
-      function onYouTubeIframeAPIReady() {
-        search();
-        currentVid=localStorage.getItem('currentVid');
-        player = new YT.Player('player', {
-          height: '390',
-          width: '640',
-          videoId: currentVid,
-          events: {
+function changeVidId(id, i) {
+    $('#player').attr('src', 'https://www.youtube.com/embed/' + id)
+    $('#player').attr('num', i)
+
+}
+
+// 2. This code loads the IFrame Player API code asynchronously.
+var tag = document.createElement('script');
+
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+// 3. This function creates an <iframe> (and YouTube player)
+//    after the API code downloads.
+var player;
+
+function onYouTubeIframeAPIReady() {
+    search();
+    currentVid = localStorage.getItem('currentVid');
+    player = new YT.Player('player', {
+        height: '390',
+        width: '640',
+        videoId: currentVid,
+        events: {
             'onReady': onPlayerReady,
             'onStateChange': onPlayerStateChange
-          }
-        });
-      }
-
-      // 4. The API will call this function when the video player is ready.
-      function onPlayerReady(event) {
-        event.target.playVideo();
-      }
-
-      // 5. The API calls this function when the player's state changes.
-      //    The function indicates that when playing a video (state=1),
-      //    the player should play for six seconds and then stop.
-      var done = false;
-      function onPlayerStateChange(event) {
-        if (event.data == YT.PlayerState.PLAYING && !done) {
-          setTimeout(stopVideo, 6000);
-          done = true;
         }
-      }
-      function stopVideo() {
-        player.stopVideo();
-      }
+    });
+}
+
+// 4. The API will call this function when the video player is ready.
+function onPlayerReady(event) {
+    event.target.playVideo();
+}
+
+// 5. The API calls this function when the player's state changes.
+//    The function indicates that when playing a video (state=1),
+//    the player should play for six seconds and then stop.
+var done = false;
+
+function onPlayerStateChange(event) {
+    if (event.data == YT.PlayerState.PLAYING && !done) {
+        setTimeout(stopVideo, 6000);
+        done = true;
+    }
+}
+
+function stopVideo() {
+    player.stopVideo();
+}
 
 
-$(document).ready(function(){
-   
- // var team=Cookies.get('currentTeam');
+$(document).ready(function() {
+
+    // var team=Cookies.get('currentTeam');
 
 
 
 
-    $('.teamList a').click(function(){
-          Cookies.set('currentTeam',  $(this).text());
-  });
-   
+    $('.teamList a').click(function() {
+        Cookies.set('currentTeam', $(this).text());
+    });
+
 
 });
 
@@ -136,161 +139,162 @@ $(document).ready(function(){
  * Copyright 2006, 2015 Klaus Hartl & Fagner Brack
  * Released under the MIT license
  */
-;(function (factory) {
-	var registeredInModuleLoader = false;
-	if (typeof define === 'function' && define.amd) {
-		define(factory);
-		registeredInModuleLoader = true;
-	}
-	if (typeof exports === 'object') {
-		module.exports = factory();
-		registeredInModuleLoader = true;
-	}
-	if (!registeredInModuleLoader) {
-		var OldCookies = window.Cookies;
-		var api = window.Cookies = factory();
-		api.noConflict = function () {
-			window.Cookies = OldCookies;
-			return api;
-		};
-	}
-}(function () {
-	function extend () {
-		var i = 0;
-		var result = {};
-		for (; i < arguments.length; i++) {
-			var attributes = arguments[ i ];
-			for (var key in attributes) {
-				result[key] = attributes[key];
-			}
-		}
-		return result;
-	}
+;
+(function(factory) {
+    var registeredInModuleLoader = false;
+    if (typeof define === 'function' && define.amd) {
+        define(factory);
+        registeredInModuleLoader = true;
+    }
+    if (typeof exports === 'object') {
+        module.exports = factory();
+        registeredInModuleLoader = true;
+    }
+    if (!registeredInModuleLoader) {
+        var OldCookies = window.Cookies;
+        var api = window.Cookies = factory();
+        api.noConflict = function() {
+            window.Cookies = OldCookies;
+            return api;
+        };
+    }
+}(function() {
+    function extend() {
+        var i = 0;
+        var result = {};
+        for (; i < arguments.length; i++) {
+            var attributes = arguments[i];
+            for (var key in attributes) {
+                result[key] = attributes[key];
+            }
+        }
+        return result;
+    }
 
-	function init (converter) {
-		function api (key, value, attributes) {
-			var result;
-			if (typeof document === 'undefined') {
-				return;
-			}
+    function init(converter) {
+        function api(key, value, attributes) {
+            var result;
+            if (typeof document === 'undefined') {
+                return;
+            }
 
-			// Write
+            // Write
 
-			if (arguments.length > 1) {
-				attributes = extend({
-					path: '/'
-				}, api.defaults, attributes);
+            if (arguments.length > 1) {
+                attributes = extend({
+                    path: '/'
+                }, api.defaults, attributes);
 
-				if (typeof attributes.expires === 'number') {
-					var expires = new Date();
-					expires.setMilliseconds(expires.getMilliseconds() + attributes.expires * 864e+5);
-					attributes.expires = expires;
-				}
+                if (typeof attributes.expires === 'number') {
+                    var expires = new Date();
+                    expires.setMilliseconds(expires.getMilliseconds() + attributes.expires * 864e+5);
+                    attributes.expires = expires;
+                }
 
-				// We're using "expires" because "max-age" is not supported by IE
-				attributes.expires = attributes.expires ? attributes.expires.toUTCString() : '';
+                // We're using "expires" because "max-age" is not supported by IE
+                attributes.expires = attributes.expires ? attributes.expires.toUTCString() : '';
 
-				try {
-					result = JSON.stringify(value);
-					if (/^[\{\[]/.test(result)) {
-						value = result;
-					}
-				} catch (e) {}
+                try {
+                    result = JSON.stringify(value);
+                    if (/^[\{\[]/.test(result)) {
+                        value = result;
+                    }
+                } catch (e) {}
 
-				if (!converter.write) {
-					value = encodeURIComponent(String(value))
-						.replace(/%(23|24|26|2B|3A|3C|3E|3D|2F|3F|40|5B|5D|5E|60|7B|7D|7C)/g, decodeURIComponent);
-				} else {
-					value = converter.write(value, key);
-				}
+                if (!converter.write) {
+                    value = encodeURIComponent(String(value))
+                        .replace(/%(23|24|26|2B|3A|3C|3E|3D|2F|3F|40|5B|5D|5E|60|7B|7D|7C)/g, decodeURIComponent);
+                } else {
+                    value = converter.write(value, key);
+                }
 
-				key = encodeURIComponent(String(key));
-				key = key.replace(/%(23|24|26|2B|5E|60|7C)/g, decodeURIComponent);
-				key = key.replace(/[\(\)]/g, escape);
+                key = encodeURIComponent(String(key));
+                key = key.replace(/%(23|24|26|2B|5E|60|7C)/g, decodeURIComponent);
+                key = key.replace(/[\(\)]/g, escape);
 
-				var stringifiedAttributes = '';
+                var stringifiedAttributes = '';
 
-				for (var attributeName in attributes) {
-					if (!attributes[attributeName]) {
-						continue;
-					}
-					stringifiedAttributes += '; ' + attributeName;
-					if (attributes[attributeName] === true) {
-						continue;
-					}
-					stringifiedAttributes += '=' + attributes[attributeName];
-				}
-				return (document.cookie = key + '=' + value + stringifiedAttributes);
-			}
+                for (var attributeName in attributes) {
+                    if (!attributes[attributeName]) {
+                        continue;
+                    }
+                    stringifiedAttributes += '; ' + attributeName;
+                    if (attributes[attributeName] === true) {
+                        continue;
+                    }
+                    stringifiedAttributes += '=' + attributes[attributeName];
+                }
+                return (document.cookie = key + '=' + value + stringifiedAttributes);
+            }
 
-			// Read
+            // Read
 
-			if (!key) {
-				result = {};
-			}
+            if (!key) {
+                result = {};
+            }
 
-			// To prevent the for loop in the first place assign an empty array
-			// in case there are no cookies at all. Also prevents odd result when
-			// calling "get()"
-			var cookies = document.cookie ? document.cookie.split('; ') : [];
-			var rdecode = /(%[0-9A-Z]{2})+/g;
-			var i = 0;
+            // To prevent the for loop in the first place assign an empty array
+            // in case there are no cookies at all. Also prevents odd result when
+            // calling "get()"
+            var cookies = document.cookie ? document.cookie.split('; ') : [];
+            var rdecode = /(%[0-9A-Z]{2})+/g;
+            var i = 0;
 
-			for (; i < cookies.length; i++) {
-				var parts = cookies[i].split('=');
-				var cookie = parts.slice(1).join('=');
+            for (; i < cookies.length; i++) {
+                var parts = cookies[i].split('=');
+                var cookie = parts.slice(1).join('=');
 
-				if (cookie.charAt(0) === '"') {
-					cookie = cookie.slice(1, -1);
-				}
+                if (cookie.charAt(0) === '"') {
+                    cookie = cookie.slice(1, -1);
+                }
 
-				try {
-					var name = parts[0].replace(rdecode, decodeURIComponent);
-					cookie = converter.read ?
-						converter.read(cookie, name) : converter(cookie, name) ||
-						cookie.replace(rdecode, decodeURIComponent);
+                try {
+                    var name = parts[0].replace(rdecode, decodeURIComponent);
+                    cookie = converter.read ?
+                        converter.read(cookie, name) : converter(cookie, name) ||
+                        cookie.replace(rdecode, decodeURIComponent);
 
-					if (this.json) {
-						try {
-							cookie = JSON.parse(cookie);
-						} catch (e) {}
-					}
+                    if (this.json) {
+                        try {
+                            cookie = JSON.parse(cookie);
+                        } catch (e) {}
+                    }
 
-					if (key === name) {
-						result = cookie;
-						break;
-					}
+                    if (key === name) {
+                        result = cookie;
+                        break;
+                    }
 
-					if (!key) {
-						result[name] = cookie;
-					}
-				} catch (e) {}
-			}
+                    if (!key) {
+                        result[name] = cookie;
+                    }
+                } catch (e) {}
+            }
 
-			return result;
-		}
+            return result;
+        }
 
-		api.set = api;
-		api.get = function (key) {
-			return api.call(api, key);
-		};
-		api.getJSON = function () {
-			return api.apply({
-				json: true
-			}, [].slice.call(arguments));
-		};
-		api.defaults = {};
+        api.set = api;
+        api.get = function(key) {
+            return api.call(api, key);
+        };
+        api.getJSON = function() {
+            return api.apply({
+                json: true
+            }, [].slice.call(arguments));
+        };
+        api.defaults = {};
 
-		api.remove = function (key, attributes) {
-			api(key, '', extend(attributes, {
-				expires: -1
-			}));
-		};
+        api.remove = function(key, attributes) {
+            api(key, '', extend(attributes, {
+                expires: -1
+            }));
+        };
 
-		api.withConverter = init;
+        api.withConverter = init;
 
-		return api;
-	}
+        return api;
+    }
 
-	return init(function () {});
+    return init(function() {});
 }));
